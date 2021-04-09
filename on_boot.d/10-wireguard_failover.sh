fc93523c903a773c52630997748938992fd6618d
k-a-s-c-h <github@anyemail.de>
2021-04-09 22:31:27 +0200

    Update 10-wireguard_failover.sh

--- on_boot.d/10-wireguard_failover.sh
+++ on_boot.d/10-wireguard_failover.sh
@@ -2,15 +2,15 @@
 while sleep 240
 do
 	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | cut -d":" -f9 | tail -n1 | sed 's/)//g'` = up ]; then
-		if [ -f /tmp/failover ]; then
+		if [ -f /tmp/failover_up ]; then
 			wg-quick down wg0
 			sleep 5
 			wg-quick up wg0
-			rm /tmp/failover
+			rm /tmp/failover_up
 		fi
 	else
-		if [ ! -f /tmp/failover ]; then
-			touch /tmp/failover
+		if [ ! -f /tmp/failover_up ]; then
+			touch /tmp/failover_up
 		fi
 	fi
 done
