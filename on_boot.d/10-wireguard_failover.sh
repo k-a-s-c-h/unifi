f10a292b1d4b76cbdaffd0e7ff16702f3304b948
k-a-s-c-h <github@anyemail.de>
2021-04-10 09:31:08 +0200

    Update 10-wireguard_failover.sh

--- on_boot.d/10-wireguard_failover.sh
+++ on_boot.d/10-wireguard_failover.sh
@@ -5,63 +5,16 @@
 
 while sleep 240
 do
-	
-wan1=primary
-wan2=none
-ulte=backup
 
-if [ $wan1 = primary ] && [ $ulte = backup ]; then
-	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $17}' | awk '{print substr($0,0,length($0)-1)}'` = up ]; then
-		[ ! -z "$failover_up" ] || failover_up=0
-		if [ $failover_up = 1 ]; then
-			wg-quick down wg0
-			sleep 5
-			wg-quick up wg0
-			unset failover_up
-		fi
-	else
-		failover_up = 1
+if [`cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $11}'` = 201 ]; then
+	[ ! -z "$failover_up" ] || failover_up=0
+	if [ $failover_up = 1 ]; then
+		wg-quick down wg0
+		wg-quick up wg0
+		unset failover_up
 	fi
-fi
-
-if [ $wan1 = primary ] && [ $wan2 = backup ]; then
-	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $17}' | awk '{print substr($0,0,length($0)-1)}'` = up ]; then
-		[ ! -z "$failover_up" ] || failover_up=0
-		if [ $failover_up = 1 ]; then
-			wg-quick down wg0
-			sleep 5
-			wg-quick up wg0
-			unset failover_up
-		fi
-	else
-		failover_up = 1
-	fi
-fi
-
-if [ $wan2 = primary ] && [ $ulte = backup ]; then
-	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $13}' | awk '{print substr($0,0,length($0)-1)}'` = up ]; then
-		[ ! -z "$failover_up" ] || failover_up=0
-		if [ $failover_up = 1 ]; then
-			wg-quick down wg0
-			sleep 5
-			wg-quick up wg0
-			unset failover_up
-		fi
-	else
-		failover_up = 1
-	fi
-fi
-
-if [ $wan2 = primary ] && [ $wan1 = backup ]; then
-	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $13}' | awk '{print substr($0,0,length($0)-1)}'` = up ]; then
-		[ ! -z "$failover_up" ] || failover_up=0
-		if [ $failover_up = 1 ]; then
-			wg-quick down wg0
-			sleep 5
-			wg-quick up wg0
-			unset failover_up
-		fi
-	else
+else
+	if [`cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $11}'` = 202 ] || [`cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $11}'` = 203 ]; then
 		failover_up = 1
 	fi
 fi
