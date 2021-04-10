63458249808c91ed290511848ca92ba74d66def8
k-a-s-c-h <github@anyemail.de>
2021-04-10 14:46:30 +0200

    Create 10-wireguard.sh

---
+++ on_boot.d/10-wireguard.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# @k-a-s-c-h on GitHub
+# @0815 on ubnt forums
+# file is located at https://github.com/k-a-s-c-h/unifi/blob/main/on_boot.d/10-wireguard.sh
+#
+# curl -LJo /mnt/data/on_boot.d/10-wireguard.sh https://raw.githubusercontent.com/k-a-s-c-h/unifi/main/on_boot.d/10-wireguard.sh
+# chmod +x /mnt/data/on_boot.d/10-wireguard.sh
+
+if [ -f /mnt/data/wireguard/setup_wireguard.sh ]; then
+	cd /mnt/data/wireguard
+	./setup_wireguard.sh
+	sleep 60
+	wg-quick up wg0
+fi
