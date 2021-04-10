0ba61a5e9daaed3e20f6f7ec2b622f99748f3a76
k-a-s-c-h <github@anyemail.de>
2021-04-10 07:39:19 +0200

    Update 10-wireguard_failover.sh

--- on_boot.d/10-wireguard_failover.sh
+++ on_boot.d/10-wireguard_failover.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 while sleep 240
 do
+	
+wan1=primary
+wan2=none
+ulte=backup
+
+if [ $wan1=primary ] && [ $ulte = backup ]; then
 	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | cut -d":" -f9 | tail -n1 | sed 's/)//g'` = up ]; then
 		if [ -f /tmp/failover_up ]; then
 			wg-quick down wg0
@@ -13,4 +19,50 @@ do
 			touch /tmp/failover_up
 		fi
 	fi
+fi
+
+if [ $wan1=primary ] && [ $wan2 = backup ]; then
+	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | cut -d":" -f9 | tail -n1 | sed 's/)//g'` = up ]; then
+		if [ -f /tmp/failover_up ]; then
+			wg-quick down wg0
+			sleep 5
+			wg-quick up wg0
+			rm /tmp/failover_up
+		fi
+	else
+		if [ ! -f /tmp/failover_up ]; then
+			touch /tmp/failover_up
+		fi
+	fi
+fi
+
+if [ $wan2=primary ] && [ $ulte = backup ]; then
+	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | cut -d":" -f7 | tail -n1 | sed 's/, gre1//g'` = up ]; then
+		if [ -f /tmp/failover_up ]; then
+			wg-quick down wg0
+			sleep 5
+			wg-quick up wg0
+			rm /tmp/failover_up
+		fi
+	else
+		if [ ! -f /tmp/failover_up ]; then
+			touch /tmp/failover_up
+		fi
+	fi
+fi
+
+if [ $wan2=primary ] && [ $wan1 = backup ]; then
+	if [ `cat /var/log/messages | grep wanFailover | grep "using table" | cut -d":" -f7 | tail -n1 | sed 's/, gre1//g'` = up ]; then
+		if [ -f /tmp/failover_up ]; then
+			wg-quick down wg0
+			sleep 5
+			wg-quick up wg0
+			rm /tmp/failover_up
+		fi
+	else
+		if [ ! -f /tmp/failover_up ]; then
+			touch /tmp/failover_up
+		fi
+	fi
+fi
 done
