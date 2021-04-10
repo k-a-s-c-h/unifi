369a53401d3b7ad03cd648cfd253a03ac3fbef4d
k-a-s-c-h <github@anyemail.de>
2021-04-10 15:07:49 +0200

    Update 10-wireguard.sh

--- on_boot.d/10-wireguard.sh
+++ on_boot.d/10-wireguard.sh
@@ -6,27 +6,22 @@
 # curl -LJo /mnt/data/on_boot.d/10-wireguard.sh https://raw.githubusercontent.com/k-a-s-c-h/unifi/main/on_boot.d/10-wireguard.sh
 # chmod +x /mnt/data/on_boot.d/10-wireguard.sh
 
-[ ! -f /usr/bin/wg-quick ] && ln -s /mnt/data/wireguard/usr/bin/wg-quick /usr/bin/wg-quick
-[ ! -f /usr/bin/wg ] && ln -s /mnt/data/wireguard/usr/bin/wg /usr/bin/wg
-[ ! -f /usr/bin/bash ] && ln -s /mnt/data/wireguard/usr/bin/bash /usr/bin/bash
-
-[ ! -d /etc/wireguard ] && ln -s /mnt/data/wireguard/etc/wireguard /etc/wireguard
-
-[ ! -d /dev/fd ] && ln -s /proc/self/fd /dev/fd &>/dev/null > /dev/null 2>&1
-
-[ `lsmod | egrep ^ip6_udp_tunnel | wc -l` = 0 ] && modprobe ip6_udp_tunnel
-[ `lsmod | egrep ^udp_tunnel | wc -l` = 0 ] && modprobe udp_tunnel
-
-lsmod | egrep ^wireguard | wc -l > /dev/null 2>&1
-if [ `lsmod | egrep ^wireguard | wc -l` = 1 ]; then
-	ver=`uname -r`
-	echo "loading wireguard..."
-	insmod /mnt/data/wireguard/wireguard-$ver.ko
-	# iptable_raw required for wg-quick's use of iptables-restore
-	insmod /mnt/data/wireguard/iptable_raw-$ver.ko
-fi
-
-if [ -f /usr/bin/wg-quick ]; then
-	sleep 60
-	wg-quick up wg0
+if [ -d /mnt/data/wireguard/ ]; then
+	[ ! -f /usr/bin/wg-quick ] && ln -s /mnt/data/wireguard/usr/bin/wg-quick /usr/bin/wg-quick
+	[ ! -f /usr/bin/wg ] && ln -s /mnt/data/wireguard/usr/bin/wg /usr/bin/wg
+	[ ! -f /usr/bin/bash ] && ln -s /mnt/data/wireguard/usr/bin/bash /usr/bin/bash
+	[ ! -d /etc/wireguard ] && ln -s /mnt/data/wireguard/etc/wireguard /etc/wireguard
+	[ ! -d /dev/fd ] && ln -s /proc/self/fd /dev/fd &>/dev/null > /dev/null 2>&1
+	[ `lsmod | egrep ^ip6_udp_tunnel | wc -l` = 0 ] && modprobe ip6_udp_tunnel
+	[ `lsmod | egrep ^udp_tunnel | wc -l` = 0 ] && modprobe udp_tunnel
+	if [ `lsmod | egrep ^wireguard | wc -l` = 1 ]; then
+		ver=`uname -r`
+		echo "loading wireguard..."
+		insmod /mnt/data/wireguard/wireguard-$ver.ko
+		insmod /mnt/data/wireguard/iptable_raw-$ver.ko
+	fi
+	if [ -f /usr/bin/wg-quick ]; then
+		sleep 60
+		wg-quick up wg0
+	fi
 fi
