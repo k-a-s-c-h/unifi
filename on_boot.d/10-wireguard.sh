1b7159c7480a3b7788a2702392c1541de4f6448c
k-a-s-c-h <github@anyemail.de>
2021-04-10 15:00:56 +0200

    Update 10-wireguard.sh

--- on_boot.d/10-wireguard.sh
+++ on_boot.d/10-wireguard.sh
@@ -6,9 +6,26 @@
 # curl -LJo /mnt/data/on_boot.d/10-wireguard.sh https://raw.githubusercontent.com/k-a-s-c-h/unifi/main/on_boot.d/10-wireguard.sh
 # chmod +x /mnt/data/on_boot.d/10-wireguard.sh
 
-if [ -f /mnt/data/wireguard/setup_wireguard.sh ]; then
-	cd /mnt/data/wireguard
-	./setup_wireguard.sh
-	sleep 60
-	wg-quick up wg0
+[ ! -f /usr/bin/wg-quick ] && ln -s /mnt/data/wireguard/usr/bin/wg-quick /usr/bin/wg-quick
+[ ! -f /usr/bin/wg ] && ln -s /mnt/data/wireguard/usr/bin/wg /usr/bin/wg
+[ ! -f /usr/bin/bash ] && ln -s /mnt/data/wireguard/usr/bin/bash /usr/bin/bash
+
+[ ! -d /etc/wireguard ] && ln -s /mnt/data/wireguard/etc/wireguard /etc/wireguard
+
+[ ! -d /dev/fd ] && ln -s /proc/self/fd /dev/fd &>/dev/null > /dev/null 2>&1
+
+[ `lsmod | egrep ^ip6_udp_tunnel | wc -l` = 0 ] && modprobe ip6_udp_tunnel
+[ `lsmod | egrep ^udp_tunnel | wc -l` = 0 ] && modprobe udp_tunnel
+
+lsmod | egrep ^wireguard | wc -l > /dev/null 2>&1
+if [ `lsmod | egrep ^wireguard | wc -l` = 1 ]; then
+	ver=`uname -r`
+	echo "loading wireguard..."
+	insmod /mnt/data/wireguard/wireguard-$ver.ko
+	# iptable_raw required for wg-quick's use of iptables-restore
+	insmod /mnt/data/wireguard/iptable_raw-$ver.ko
 fi
+
+sleep 60
+
+wg-quick up wg0
