4e97c32cdcaab153433a2fedea38e3db7b0d2b80
k-a-s-c-h <82236526+k-a-s-c-h@users.noreply.github.com>
2021-04-16 00:13:20 +0200

    Update and rename 11-ad-ula.sh to 11-add-ula.sh

--- on_boot.d/11-add-ula.sh
+++ on_boot.d/11-add-ula.sh
@@ -2,8 +2,8 @@
 # @k-a-s-c-h on GitHub
 # @0815 on ubnt forums
 # file is located at https://github.com/k-a-s-c-h/unifi/blob/main/on_boot.d/11-ad-ula.sh
-# curl -LJo /mnt/data/on_boot.d/11-ad-ula.sh https://raw.githubusercontent.com/k-a-s-c-h/unifi/main/on_boot.d/11-ad-ula.sh
-# chmod +x /mnt/data/on_boot.d/11-ad-ula.sh
+# curl -LJo /mnt/data/on_boot.d/11-add-ula.sh https://raw.githubusercontent.com/k-a-s-c-h/unifi/main/on_boot.d/11-add-ula.sh
+# chmod +x /mnt/data/on_boot.d/11-add-ula.sh
 # execute via ssh "ifconfig br0 | grep HWaddr | awk '{print $5}'" and generate a ULA PREFIX with your br0 MAC address on https://cd34.com/rfc4193/ and enter it under ULA_PREFIX_br0
 ###############
 logfile=0
