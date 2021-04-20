98c4d3c91e0fb3d2f2803bc0eac0ac2fc3f50644
k-a-s-c-h <82236526+k-a-s-c-h@users.noreply.github.com>
2021-04-20 18:54:13 +0200

    Update 12-inwx_dyndns.sh

--- on_boot.d/12-inwx_dyndns.sh
+++ on_boot.d/12-inwx_dyndns.sh
@@ -14,21 +14,24 @@ update_sleeptime=30
 update_ipv4ipv6=true
 update_ipv4_only=false
 update_ipv6_only=false
+namesver=ns.inwx.de
+get_ipv4=ifconfig.co
+get_ipv6=ifconfig.co
 ###############
 
 while sleep $update_sleeptime
 do
 
 	if [ $update_ipv4ipv6 = true ]; then
-		if [ `nslookup -type=a $dyndns_hostname ns.inwx.de | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -4 ifconfig.co` ] || [ `nslookup -type=aaaa $dyndns_hostname ns.inwx.de | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -6 ifconfig.co` ]; then
+		if [ `nslookup -type=a $dyndns_hostname $nameserver | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -4 $get_ipv4` ] || [ `nslookup -type=aaaa $dyndns_hostname $nameserver | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -6 $get_ipv6` ]; then
 			curl --user $dyndns_username:$dyndns_password "https://dyndns.inwx.com/nic/update?myip=${ipv4}&myipv6=${ipv6}"
 		fi
 	elif [ $update_ipv4_only = true ]; then
-		if [ `nslookup -type=a $dyndns_hostname ns.inwx.de | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -4 ifconfig.co` ]; then
+		if [ `nslookup -type=a $dyndns_hostname $nameserver | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -4 $get_ipv4` ]; then
 			curl --user $dyndns_username:$dyndns_password "https://dyndns.inwx.com/nic/update?myip=${ipv4}"
 		fi
 	elif [ $update_ipv6_only = true ]; then
-		if [ `nslookup -type=aaaa $dyndns_hostname ns.inwx.de | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -6 ifconfig.co` ]; then
+		if [ `nslookup -type=aaaa $dyndns_hostname $nameserver | tail -n2 | grep Address | awk '{print $2}'` != `curl -s -6 $get_ipv6` ]; then
 			curl --user $dyndns_username:$dyndns_password "https://dyndns.inwx.com/nic/update?myipv6=${ipv6}"
 		fi
 	fi
