3bb6ce56d44ad756a2036b7afaacf9367969a48a
k-a-s-c-h <github@anyemail.de>
2021-08-29 13:14:41 +0200

    Update 10-wireguard_failover.sh

--- on_boot.d/10-wireguard_failover.sh
+++ on_boot.d/10-wireguard_failover.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # @k-a-s-c-h on GitHub
 # @0815 on ubnt forums
 # file is located at https://github.com/k-a-s-c-h/unifi/blob/main/on_boot.d/10-wireguard_failover.sh
@@ -11,6 +11,7 @@
 ###############
 logfile=0
 sleeptime=30
+wireguard_interface=("wg0")
 ###############
 
 [ ! -z "$sleep_time" ] || sleep_time=0
@@ -18,27 +19,31 @@ sleeptime=30
 while sleep $sleep_time
 do
 
-if [ -f /etc/wireguard/wg0.conf ]; then
-	using_table=`cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $11}'`
-	if [ $using_table = 201 ]; then
-		sleep_time=$sleeptime
-		[ ! -z "$failover" ] || failover=0
-		if [ $failover = 1 ]; then
-			wg-quick down wg0
-			wg-quick up wg0
-			unset failover
-			[ $logfile = 1 ] && echo "$(date +%F_%H-%M-%S) WAN" >> /mnt/data/log/wireguard_failover
-		fi
-	else
-		if [ $using_table = 202 ] || [ $using_table = 203 ]; then
-			if [ $logfile = 1 ]; then
-				[ ! -z "$failover" ] || failover=0
-				[ $failover = 0 ] && echo "$(date +%F_%H-%M-%S) WAN_Failover" >> /mnt/data/log/wireguard_failover
+	for config in "${wireguard_interface[@]}"
+		do
+			if [ -f /etc/wireguard/$wireguard_interface.conf ]; then
+				using_table=`cat /var/log/messages | grep wanFailover | grep "using table" | tail -n1 | awk '{print $11}'`
+				if [ $using_table = 201 ]; then
+					sleep_time=$sleeptime
+					[ ! -z "$failover" ] || failover=0
+					if [ $failover = 1 ]; then
+						wg-quick down $wireguard_interface
+						wg-quick up $wireguard_interface
+						unset failover
+						[ $logfile = 1 ] && echo "$(date +%F_%H-%M-%S) WAN" >> /mnt/data/log/wireguard_failover
+					fi
+				else
+					if [ $using_table = 202 ] || [ $using_table = 203 ]; then
+						if [ $logfile = 1 ]; then
+							[ ! -z "$failover" ] || failover=0
+							[ $failover = 0 ] && echo "$(date +%F_%H-%M-%S) WAN_Failover" >> /mnt/data/log/wireguard_failover
+						fi
+						sleep_time=$sleeptime
+						failover=1
+					fi
+				fi
 			fi
-			sleep_time=$sleeptime
-			failover=1
-		fi
-	fi
-fi
+
+		done
 
 done
